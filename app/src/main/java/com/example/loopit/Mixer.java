42effea22f55e4ec26bafc67c508675108733868
Vishal Venkatraman <ihandsomeguy97@gmail.com>
2019-04-28 12:21:39 +0800

    Minor UI Tweak - Main Activity

---
+++ app/src/main/java/com/example/loopit/Mixer.java
@@ -0,0 +1,12 @@
+package com.example.loopit;
+
+import be.tarsos.dsp.AudioDispatcher;
+import be.tarsos.dsp.AudioEvent;
+import be.tarsos.dsp.io.android.AudioDispatcherFactory;
+import be.tarsos.dsp.pitch.PitchDetectionHandler;
+import be.tarsos.dsp.pitch.PitchDetectionResult;
+import be.tarsos.dsp.pitch.PitchProcessor;
+import be.tarsos.dsp.synthesis.PitchResyntheziser;
+
+public class Mixer {
+}
